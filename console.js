7376156b6c458ae7b994241a8f7f4354790b6c5c
Bryan Jeanty <bryan.jeanty@outlook.com>
2019-05-24 23:41:20 -0400

    console methods examined

---
+++ console.js
@@ -0,0 +1,36 @@
+// log to console
+// log a string
+console.log("Hello World");
+// log numbers
+console.log(123);
+// log booleans
+console.log(true);
+// create and log variables
+var greeting = "hello";
+console.log(greeting);
+// log arrays
+console.log([1, 2, 3, 4]);
+// log objects
+console.log({ a: 1, b: 2 });
+// log objects as tables
+console.table({ a: 1, b: 2 });
+// log to error stream
+console.error("This is some error");
+// clear the console
+console.clear();
+// log to warning stream
+console.warn("This is some warning");
+// start console timer
+console.time();
+for (let i = 0; i < 10; i++) {
+  console.log("Hello World");
+}
+// end console timer
+console.timeEnd();
+
+/*
+  multi
+  line
+  comments
+  ftw!
+*/
